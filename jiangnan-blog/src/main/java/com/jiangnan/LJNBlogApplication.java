80afa9981e4750113e2f87556b308f38b8655458
Andy <Andy@375.com>
2024-06-19 17:50:52 +0800

    jiangnanBlog - 2

--- jiangnan-blog/src/main/java/com/jiangnan/LJNBlogApplication.java
+++ jiangnan-blog/src/main/java/com/jiangnan/LJNBlogApplication.java
@@ -1,7 +1,11 @@
 package com.jiangnan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LJNBlogApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(LJNBlogApplication.class, args);
     }
 }
